543e2023eb83fb457fade1fa4eaeea37d3400b46
a <a@tuxpa.in>
2024-09-12 23:23:29 -0500

    Update entries.ts

--- entries.ts
+++ entries.ts
@@ -1,10 +1,5 @@
 import { Item } from "feed";
 
 
-export const feedItems:Item[] = [{
-  date: new Date(2024, 8, 26),
-  link: 'https://example.com',
-  title: 'example custom feed item',
-  content: 'some example content for the news article'
-}]
+export const feedItems:Item[] = []
 
